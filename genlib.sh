a6ab8f5e180f62ddff8f27c24f3635ce8577356d
mathiasbredholt <mathiasbredholt@gmail.com>
2020-05-27 15:53:09 -0400

    Removed duplicate generation of libmapper.a

--- genlib.sh
+++ genlib.sh
@@ -2,10 +2,7 @@
 SRC_DIR=../
 OUTPUT_DIR=Arduino/libmapper
 OUTPUT_SRC_DIR=$OUTPUT_DIR/src
-OUTPUT_LIB_DIR=$OUTPUT_SRC_DIR/esp32
 mkdir -p $OUTPUT_DIR
-mkdir -p $OUTPUT_LIB_DIR
-cp libmapper.a $OUTPUT_LIB_DIR/libmapper.a
 cp $SRC_DIR/library.properties $OUTPUT_DIR/library.properties
 cp -R $SRC_DIR/examples $OUTPUT_DIR
 cp $SRC_DIR/mapper.h $OUTPUT_SRC_DIR/mapper.h
